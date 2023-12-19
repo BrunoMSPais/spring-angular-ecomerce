e0a20615f51a8484683014d5847321437f0a3157
BrunoMSPais <bpais@outlook.pt>
2023-12-19 09:29:42 +0000

    backend start

---
+++ backend/src/test/java/com/capgeminiprerwork/ecommercebackend/SpringBootEcommerceBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.capgeminiprerwork.ecommercebackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootEcommerceBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
