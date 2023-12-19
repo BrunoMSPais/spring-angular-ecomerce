7824e2d214422238bf91fc16b33c9eb2d9e9e2ec
BrunoMSPais <bpais@outlook.pt>
2023-12-19 12:12:31 +0000

    Added CORS support

--- backend/src/main/java/com/capgeminiprerwork/ecommercebackend/dao/ProductRepository.java
+++ backend/src/main/java/com/capgeminiprerwork/ecommercebackend/dao/ProductRepository.java
@@ -2,6 +2,8 @@ package com.capgeminiprerwork.ecommercebackend.dao;
 
 import com.capgeminiprerwork.ecommercebackend.entity.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
+@CrossOrigin("http://localhost:4200") // allow Angular app to access this endpoint
 public interface ProductRepository extends JpaRepository<Product, Long> {
 }
