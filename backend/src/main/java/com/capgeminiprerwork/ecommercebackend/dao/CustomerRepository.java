5921d51c0cadb8fc5cc36326072cdcb5224365f9
BrunoMSPais <bpais@outlook.pt>
2024-01-02 10:41:09 +0000

    backend - refactoring: handling customer by email to avoid data replication when making a new order

--- backend/src/main/java/com/capgeminiprerwork/ecommercebackend/dao/CustomerRepository.java
+++ backend/src/main/java/com/capgeminiprerwork/ecommercebackend/dao/CustomerRepository.java
@@ -4,4 +4,5 @@ import com.capgeminiprerwork.ecommercebackend.entity.Customer;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface CustomerRepository extends JpaRepository<Customer, Long> {
+    Customer findByEmail(String theEmail);
 }
